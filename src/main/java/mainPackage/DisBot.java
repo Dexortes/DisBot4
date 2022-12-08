a34d4e27c643f690a556a1be67c6caa12f08f893
Dexortes <dexortes@gmail.com>
2022-12-08 19:25:33 +0600

    .env created token is hidden

--- src/main/java/mainPackage/DisBot.java
+++ src/main/java/mainPackage/DisBot.java
@@ -1,5 +1,6 @@
 package mainPackage;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
@@ -9,15 +10,22 @@ import javax.security.auth.login.LoginException;
 
 public class DisBot {
 
+    private final Dotenv config;
     private final ShardManager shardManager;
 
     public DisBot() throws LoginException{
-        String token = "MTA0ODExNzE4MTMwMjQ1MjI3NA.GyLDRp.I3HW6CebD_UHmuUVBiXLe_mutm8aQ6MdXnGhMg";
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setActivity(Activity.playing("Study Soup World"))
         .setStatus(OnlineStatus.ONLINE);
         shardManager = builder.build();
     }
+
+    public Dotenv getConfig(){
+        return config;
+    }
+
 
     public ShardManager getShardManager() {
         return shardManager;
